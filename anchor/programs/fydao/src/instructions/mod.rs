1507a1f641094b20a8a0cf6dd5248c4752eb8229
Anurag Borkar <anuragborkar2005@gmail.com>
2026-08-09 17:46:39 +0000

    feat(fydao): export instruction modules in mod.rs

---
+++ anchor/programs/fydao/src/instructions/mod.rs
@@ -0,0 +1,46 @@
+pub mod initialize_dao;
+pub mod initialize_governance_token;
+pub mod mint_governance_tokens;
+pub mod delegate_votes;
+pub mod create_campaign;
+pub mod approve_and_go_live;
+pub mod donate;
+pub mod propose_milestone;
+pub mod release_milestone;
+pub mod emergency_withdraw;
+pub mod create_proposal;
+pub mod cast_vote;
+pub mod queue_proposal;
+pub mod execute_proposal;
+pub mod cancel_proposal;
+
+#[allow(ambiguous_glob_reexports)]
+pub use approve_and_go_live::*;
+#[allow(ambiguous_glob_reexports)]
+pub use cancel_proposal::*;
+#[allow(ambiguous_glob_reexports)]
+pub use cast_vote::*;
+#[allow(ambiguous_glob_reexports)]
+pub use create_campaign::*;
+#[allow(ambiguous_glob_reexports)]
+pub use create_proposal::*;
+#[allow(ambiguous_glob_reexports)]
+pub use delegate_votes::*;
+#[allow(ambiguous_glob_reexports)]
+pub use donate::*;
+#[allow(ambiguous_glob_reexports)]
+pub use emergency_withdraw::*;
+#[allow(ambiguous_glob_reexports)]
+pub use execute_proposal::*;
+#[allow(ambiguous_glob_reexports)]
+pub use initialize_dao::*;
+#[allow(ambiguous_glob_reexports)]
+pub use initialize_governance_token::*;
+#[allow(ambiguous_glob_reexports)]
+pub use mint_governance_tokens::*;
+#[allow(ambiguous_glob_reexports)]
+pub use propose_milestone::*;
+#[allow(ambiguous_glob_reexports)]
+pub use queue_proposal::*;
+#[allow(ambiguous_glob_reexports)]
+pub use release_milestone::*;
