5a50d322ef63b2741ca6a744e9e6ec620d4c5734
Anurag Borkar <anuragborkar2005@gmail.com>
2026-08-09 08:08:31 +0000

    feat(fydao): add state module exports

---
+++ anchor/programs/fydao/src/state/mod.rs
@@ -0,0 +1,13 @@
+pub mod campaign;
+pub mod dao_config;
+pub mod governance_token;
+pub mod milestone;
+pub mod proposal;
+pub mod vote_record;
+
+pub use campaign::*;
+pub use dao_config::*;
+pub use governance_token::*;
+pub use milestone::*;
+pub use proposal::*;
+pub use vote_record::*;
